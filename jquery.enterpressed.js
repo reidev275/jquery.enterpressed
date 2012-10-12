1a25d96a8885b2744a19776c7b1f7c1f73b3e005
libra guest <reidev275@gmail.com>
2012-10-12 12:08:30 -0400

    initial commit

---
+++ jquery.enterpressed.js
@@ -0,0 +1,11 @@
+(function ($) {
+
+    $.prototype.enterPressed = function (fn) {
+        $(this).keyup(function (e) {
+            if ((e.keyCode || e.which) == 13) {
+                fn();
+            }
+        });
+    };
+
+}(jQuery || {}));
