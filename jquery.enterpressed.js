bc95883def1bd7f210f4b98303ea0cef801d1183
libra guest <reidev275@gmail.com>
2012-10-15 12:42:54 -0400

    added unit tests

--- jquery.enterpressed.js
+++ jquery.enterpressed.js
@@ -1,11 +1,12 @@
 (function ($) {
 
-    $.prototype.enterPressed = function (fn) {
+    $.fn.enterPressed = function (func) {
         $(this).keyup(function (e) {
             if ((e.keyCode || e.which) == 13) {
-                fn();
+                func();
             }
         });
+        return this;
     };
 
 }(jQuery || {}));
