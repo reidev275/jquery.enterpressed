79efd346b49020eda53972cb251feab72b16880c
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2012-10-17 11:22:40 -0400

    Revert keypress event to keyup event because the latter captures codes for special keys as well.

--- jquery.enterpressed.js
+++ jquery.enterpressed.js
@@ -1,7 +1,7 @@
 (function ($) {
 
     $.fn.enterPressed = function (func) {
-        this.keypress(function (e) {
+        this.keyup(function (e) {
             if ((e.keyCode || e.which) === 13) {
                 func();
             }
